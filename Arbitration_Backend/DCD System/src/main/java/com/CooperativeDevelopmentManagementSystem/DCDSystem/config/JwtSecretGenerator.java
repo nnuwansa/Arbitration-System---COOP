10091afe84258fd645bb791c06f1e3ad83209273
nnuwansa <159515255+nnuwansa@users.noreply.github.com>
2026-01-12 08:58:04 +0530

    Update JwtSecretGenerator.java

--- Arbitration_Backend/DCD System/src/main/java/com/CooperativeDevelopmentManagementSystem/DCDSystem/config/JwtSecretGenerator.java
+++ Arbitration_Backend/DCD System/src/main/java/com/CooperativeDevelopmentManagementSystem/DCDSystem/config/JwtSecretGenerator.java
@@ -6,7 +6,7 @@ import java.security.SecureRandom;
 public class JwtSecretGenerator {
     public static void main(String[] args) {
         SecureRandom random = new SecureRandom();
-        byte[] bytes = new byte[64]; // 64 bytes = 512 bits
+        byte[] bytes = new byte[64]; 
         random.nextBytes(bytes);
         String secret = Base64.getEncoder().encodeToString(bytes);
         System.out.println(secret);
